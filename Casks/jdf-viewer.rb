0ca990473cfb75a2fe3e6371812cbc7c5f30dd36
uurtech <girisiyorum@gmail.com>
2026-06-11 12:32:32 +0400

    olaylar olaylar

--- Casks/jdf-viewer.rb
+++ Casks/jdf-viewer.rb
@@ -1,6 +1,6 @@
 cask "jdf-viewer" do
   version "0.1.0"
-  sha256 "5457f7f0171fcd028bd10349ea5727107ad1660dc987645c0867ceef4f7bc4a6"
+  sha256 "c12f452f687ddd47e33e05cea75f2f24fbf7d96c601e6c88a54992a7b68ad1e5"
 
   url "https://github.com/uurtech/jdf/releases/download/v#{version}/JDF.Viewer_#{version}_aarch64.dmg"
   name "JDF Viewer"
