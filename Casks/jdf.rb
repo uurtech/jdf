c0ea1f370ec40568bc9fd6ef3bd04f8466f557da
uurtech <girisiyorum@gmail.com>
2026-06-15 10:19:36 +0400

    added jfdx parts

--- Casks/jdf.rb
+++ Casks/jdf.rb
@@ -1,6 +1,6 @@
 cask "jdf" do
-  version "0.1.14"
-  sha256 "a896cd9735d6176cd2e76e71a097841e0798797d64c164ba6b0d09e77425201d"
+  version "0.1.15"
+  sha256 "5ada4d249e6e45dd466e41a534f7bbd1adebfc00096ac6f2e930e51eb6d1a39f"
 
   url "https://github.com/uurtech/jdf/releases/download/v#{version}/JDF.Reader_#{version}_aarch64.dmg"
   name "JDF Reader"
